929cbca2ed7836723d9aa5a90e85c26c603009c0
Parameswari Ettiappan <parameswaribala@gmail.com>
2026-06-24 12:10:17 +0530

    updated

--- src/azuremlapp/utils/score.py
+++ src/azuremlapp/utils/score.py
@@ -1,36 +1,41 @@
+import os
+import glob
 import joblib
 import pandas as pd
-import os
-from dotenv import load_dotenv
-env_path = os.path.join(os.path.dirname(__file__), '..', '.env')
-load_dotenv(env_path)
-model_path = os.getenv('model_path')
-artifact = joblib.load(model_path)
 
-model = artifact["model"]
-feature_columns = artifact["feature_columns"]
 
-sample = {
-    "amount": 150000,
-    "hour": 2,
-    "merchant_category": "Electronics",
-    "location": "Chennai"
-}
+def init():
+    global model
+    global feature_columns
 
-df = pd.DataFrame([sample])
+    model_dir = os.environ["AZUREML_MODEL_DIR"]
+    model_file = glob.glob(os.path.join(model_dir, "**", "*.pkl"), recursive=True)[0]
 
-df = pd.get_dummies(
-    df,
-    columns=['merchant_category', 'location'],
-    drop_first=True
-)
+    artifact = joblib.load(model_file)
+    model = artifact["model"]
+    feature_columns = artifact["feature_columns"]
 
-# Align columns with training
-df = df.reindex(
-    columns=feature_columns,
-    fill_value=0
-)
 
-prediction = model.predict(df)
+def run(mini_batch):
+    results = []
 
-print("Fraud Prediction:", prediction[0])
+    for file_path in mini_batch:
+        df = pd.read_csv(file_path)
+
+        df = pd.get_dummies(
+            df,
+            columns=["merchant_category", "location"],
+            drop_first=True
+        )
+
+        df = df.reindex(columns=feature_columns, fill_value=0)
+
+        predictions = model.predict(df)
+
+        output = pd.DataFrame({
+            "prediction": predictions
+        })
+
+        results.append(output)
+
+    return pd.concat(results, ignore_index=True)
